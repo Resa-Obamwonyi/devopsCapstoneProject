1695272b455d6b1489ad1d0518df0d20632d1428
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2023-04-28 00:37:13 +0100

    run build with env variable

--- blue/upload_docker.sh
+++ blue/upload_docker.sh
@@ -5,7 +5,7 @@ dockerpath=resaobas/capstone_blueapp
 
 # Step 2
 echo "Docker ID and Image: $dockerpath"
-docker login --username resaobas
+docker login --username resaobas --password $1
 docker tag capstone_blueapp $dockerpath
 
 # Step 3
--- green/upload_docker.sh
+++ green/upload_docker.sh
@@ -5,7 +5,7 @@ dockerpath=resaobas/capstone_greenapp
 
 # Step 2
 echo "Docker ID and Image: $dockerpath"
-docker login --username resaobas 
+docker login --username resaobas --password $1
 
 docker tag capstone_greenapp $dockerpath
 
